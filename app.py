1defcb7d9f3cc5d11c4ab5d274e964e815d23ba0
Kurezu <klezzz@list.ru>
2024-08-07 11:58:35 +0300

    edit app

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
+import time
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:12345@localhost/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:12345@localhost/testdb2'
 db = SQLAlchemy(app)
 
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
+    details = db.Column(db.String, nullable=False)
     is_done = db.Column(db.Boolean, nullable=False, default=False)
+    create_time = db.Column(db.DateTime)
+    close_time = db.Column(db.DateTime, nullable=True)
+    
+    def get_close_time(self):
+        return self.close_time.strftime('%d.%m.%Y %H:%M:%S')
+    
+    def get_create_time(self):
+        return self.create_time.strftime('%d.%m.%Y %H:%M:%S')
 
 
 @app.route('/')
@@ -20,10 +30,10 @@ def index():
 @app.route('/add', methods=["POST"])
 def add():
     text = request.form.get('task')
+    details = request.form.get('details')
     db.session.add(
-        Task(text=text)
+        Task(text=text, details=details, create_time = datetime.today())
     )
-    print (text)
     db.session.commit()
     return redirect(url_for('index'))
 
@@ -37,11 +47,21 @@ def clear():
 def done(task_id):
     task = Task.query.get(task_id)   
     task.is_done = True 
+    task.close_time = datetime.today()
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/reopen/<int:task_id>')
+def reopen(task_id):
+    task = Task.query.get(task_id)   
+    task.is_done = False 
+    task.close_time = None
     db.session.add(task)
     db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context(): db.create_all()
     app.run(debug=True)
     
